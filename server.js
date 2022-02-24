f23e38f5d53666adf014f8234739f1c0f9adc8d5
Kishan Changela <52081694+kpchangela@users.noreply.github.com>
2022-02-24 17:41:27 -0500

    changed up structure of server.js

--- server.js
+++ server.js
@@ -1,118 +1,83 @@
+//// Creating the server ////
+// Define named constants
+const START_ARG_NUM = 2
+const DEFAULT_PORT = 3000
+const HTTP_STATUS_OK = 200
+const HTTP_STATUS_NOT_FOUND = 404
+const CONTENT_TYPE_TEXT_PLAIN = 'text/plain'
+const HEADS = 'heads'
+const TAILS = 'tails'
+
+// Create Require
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url)
 
+// Require Express.js
 const express = require('express')
 const app = express()
 
+// Require minimist module to process one argument `--port=` on the command line after `node server.js`.
 const minimist = require('minimist')
 const { exit } = require('process')
 
-
-const allArguments = minimist(process.argv.slice(2))
+// Define allowed argument name 'port'.
+const allArguments = minimist(process.argv.slice(START_ARG_NUM))
 const argPort = allArguments['port']
-const port = argPort || process.env.PORT || 3000
+
+// Define a const `port` using the argument from the command line. 
+// Make this const default to port 3000 if there is no argument given for `--port`.
+const port = argPort || process.env.PORT || DEFAULT_PORT
+
+// Import the coinFlips and countFlips functions from your coin.mjs file
+import { coinFlip, coinFlips, countFlips, flipACoin } from './coin.mjs'
 
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
-});
+})
 
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-});
 
+//// API endpoints ////
+// Check endpoint
 app.get('/app/', (req, res) => {
     // Respond with status 200
-        res.statusCode = 200;
+    res.statusCode = 200
     // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
+    res.statusMessage = 'OK'
+    res.writeHead( res.statusCode, { 'Content-Type' : CONTENT_TYPE_TEXT_PLAIN })
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
 
-// Single flip
+// One flip
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
-    res.status(200).json({
+    res.status(HTTP_STATUS_OK).json({
         'flip': flip
     })
 })
 
-// Multi flip
+// Multiple flips
 app.get('/app/flips/:number', (req, res) => {
     var coinFlipsResult = coinFlips(req.params.number)
     var coinFlipsResultSummary = countFlips(coinFlipsResult)
-    res.status(200).json({
+
+    res.status(HTTP_STATUS_OK).json({
         'raw': coinFlipsResult,
         'summary': coinFlipsResultSummary
     })
 });
 
+// Flip match against heads
 app.get('/app/flip/call/heads', (req, res) => {
-    res.status(200).json(flipACoin('heads'))
+    res.status(HTTP_STATUS_OK).json(flipACoin(HEADS))
 })
 
+// Flip match against tails
 app.get('/app/flip/call/tails', (req, res) => {
-    res.status(200).json(flipACoin('tails'))
+    res.status(HTTP_STATUS_OK).json(flipACoin(TAILS))
 })
 
-const HEADS = 'heads'
-const TAILS = 'tails'
-const WIN = 'win'
-const LOSE = 'lose'
-
-function coinFlip() {
-  return (Math.round(Math.random()) == 0) ? HEADS : TAILS
-}
-
-function coinFlips(flips) {
-  var allFlips = [];
-
-  for (let i = 0; i < flips; i++) {
-    allFlips.push(coinFlip())
-  }
-
-  return allFlips
-}
-
-function countFlips(array) {
-  var summary = {
-    heads: 0,
-    tails: 0
-  }
-  // TODO: account for undefined input?
-  array.forEach(flip => {
-    if (flip === HEADS) {
-      summary.heads++
-    } else if (flip === TAILS) {
-      summary.tails++
-    }
-  })
-
-  // Remove uneccessary properties.
-  if (summary.heads == 0) {
-    delete summary.heads
-  } else if (summary.tails == 0) {
-    delete summary.tails
-  }
-
-  return summary
-}
-
-function flipACoin(call) {
-  if (call === HEADS || call === TAILS) {
-    var resultsSummary = {
-      call: call,
-      flip: coinFlip(),
-      result: null
-    }
-
-    resultsSummary.result = ((resultsSummary.call === resultsSummary.flip) ? WIN : LOSE)
-    return resultsSummary
-  } else if (call === "" || call == null) {
-    throw 'Error: no input.'
-  } else {
-    throw 'Usage: node guess-flip --call=[heads|tails]'
-  }
-}
+// Default response for any request not addressed by the defined endpoints
+app.use(function (req, res) {
+    res.status(HTTP_STATUS_NOT_FOUND).send('404 NOT FOUND')
+})
